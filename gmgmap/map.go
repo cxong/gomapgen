9ab405151cf6b9cb943e55e98906afa1882c189c
Cong <congusbongus@gmail.com>
2015-07-22 23:44:45 +1000

    move map construction into constructor

--- gmgmap/map.go
+++ gmgmap/map.go
@@ -2,46 +2,61 @@ package gmgmap
 
 import "fmt"
 
+// Map - a rectangular tile map
 type Map struct {
-  Tiles  []byte
-  Width  int
-  Height int
+	Tiles  []byte
+	Width  int
+	Height int
 }
 
+// NewMap - create a new Map for a certain size
+func NewMap(width, height int) *Map {
+	m := new(Map)
+	m.Width = width
+	m.Height = height
+	for y := 0; y < m.Height; y++ {
+		for x := 0; x < m.Width; x++ {
+			m.Tiles = append(m.Tiles, ' ')
+		}
+	}
+	return m
+}
+
+// Print - print map in ascii, with a border
 func (m Map) Print() {
-  for y := 0; y < m.Height; y++ {
-    // Upper frame
-    if y == 0 {
-      fmt.Print("+")
-      for x := 0; x < m.Width; x++ {
-        fmt.Print("-")
-      }
-      fmt.Print("+")
-      fmt.Println()
-    }
-    
-    // Left of frame
-    fmt.Print("|")
-    
-    // Interior cells
-    for x := 0; x < m.Width; x++ {
-      var i = y * m.Width + x
-      fmt.Printf("%c", m.Tiles[i])
-    }
-    
-    // Right of frame
-    fmt.Print("|")
-    
-    // Bottom frame
-    if y == m.Height - 1 {
-      fmt.Println()
-      fmt.Print("+")
-      for x := 0; x < m.Width; x++ {
-        fmt.Print("-")
-      }
-      fmt.Print("+")
-    }
-    
-    fmt.Println()
-  }
+	for y := 0; y < m.Height; y++ {
+		// Upper frame
+		if y == 0 {
+			fmt.Print("+")
+			for x := 0; x < m.Width; x++ {
+				fmt.Print("-")
+			}
+			fmt.Print("+")
+			fmt.Println()
+		}
+
+		// Left of frame
+		fmt.Print("|")
+
+		// Interior cells
+		for x := 0; x < m.Width; x++ {
+			var i = y*m.Width + x
+			fmt.Printf("%c", m.Tiles[i])
+		}
+
+		// Right of frame
+		fmt.Print("|")
+
+		// Bottom frame
+		if y == m.Height-1 {
+			fmt.Println()
+			fmt.Print("+")
+			for x := 0; x < m.Width; x++ {
+				fmt.Print("-")
+			}
+			fmt.Print("+")
+		}
+
+		fmt.Println()
+	}
 }
--- main.go
+++ main.go
@@ -3,15 +3,8 @@ package main
 import "github.com/cxong/gomapgen/gmgmap"
 
 func main() {
-  // make map
-  var m gmgmap.Map
-  m.Width = 80
-  m.Height = 24
-  for y := 0; y < m.Height; y++ {
-    for x := 0; x < m.Width; x++ {
-      m.Tiles = append(m.Tiles, ' ')
-    }
-  }
-  // print
-  m.Print()
+	// make map
+	m := gmgmap.NewMap(80, 24)
+	// print
+	m.Print()
 }
