6e4ad7433eafa3f3fe03608d99d151da7ce40315
Cong <congusbongus@gmail.com>
2015-07-24 21:55:09 +1000

    Parameterise

--- main.go
+++ main.go
@@ -7,13 +7,21 @@ import (
 )
 
 func main() {
+	algo := flag.String("algo", "rogue", "generation algorithm: walk/rogue")
 	width := flag.Int("width", 32, "map width")
 	height := flag.Int("height", 32, "map height")
-	//iterations := flag.Int("iterations", 3000, "number of iterations for random walk algo")
+	iterations := flag.Int("iterations", 3000, "number of iterations for random walk algo")
+	minRoomSize := flag.Int("minroomsize", 3, "minimum room size, for rogue algo")
+	maxRoomSize := flag.Int("maxroomsize", 15, "maximum room size, for rogue algo")
 	flag.Parse()
 	// make map
-	//m := gmgmap.NewRandomWalk(*width, *height, *iterations)
-	m := gmgmap.NewRogue(*width, *height, 3, 15)
+	m := gmgmap.NewMap(*width, *height)
+	switch *algo {
+	case "walk":
+		m = gmgmap.NewRandomWalk(*width, *height, *iterations)
+	case "rogue":
+		m = gmgmap.NewRogue(*width, *height, *minRoomSize, *maxRoomSize)
+	}
 	// print
 	m.Print()
 	// export TMX
