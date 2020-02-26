f29c8d70af6e2fb5d8210fc959662575c9489c35
Cong <congusbongus@gmail.com>
2020-02-26 21:58:43 +1100

    Place a few random buildings

--- gmgmap/village.go
+++ gmgmap/village.go
@@ -1,5 +1,7 @@
 package gmgmap
 
+import "math/rand"
+
 // NewVillage - create a village, made up of multiple buildings
 func NewVillage(width, height int) *Map {
 	m := NewMap(width, height)
@@ -10,7 +12,21 @@ func NewVillage(width, height int) *Map {
 
 	// Buildings
 	s := m.Layer("Structures")
-	s.rectangle(rect{1, 1, g.Width - 2, g.Height - 3}, wall, false)
+	for i := 0; i < 5; i++ {
+		w := rand.Intn(3) + 5
+		h := rand.Intn(3) + 5
+		x := rand.Intn(width - w)
+		y := rand.Intn(height - h)
+		if x < 0 || y < 0 {
+			continue
+		}
+		addBuilding(s, x, y, w, h)
+	}
 
 	return m
+}
+
+func addBuilding(s *Layer, x, y, w, h int) {
+	// Perimeter
+	s.rectangle(rect{x, y, w, h}, wall, false)
 }
